cee26553985920719d226c9bad4ea65fe55d5399
Mohammad-assaf <88990892+Mohammad-assaf@users.noreply.github.com>
2022-04-24 21:50:14 +0300

    add delete and edit option

--- lib/views/components/employee_bubble.dart
+++ lib/views/components/employee_bubble.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shrms/data/firestore/employees_firestore_helper.dart';
 import 'package:shrms/models/employee.dart';
 
 class EmployeeBubble extends StatelessWidget {
@@ -11,57 +12,145 @@ class EmployeeBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    EmployeeFirestoreHelper employeeFirestoreHelper = EmployeeFirestoreHelper();
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10.sp, vertical: 5.sp),
-      child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0XFF56C596),
-          borderRadius: BorderRadius.circular(20.sp),
-        ),
-        child: Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(10.sp),
-              child: const CircleAvatar(
-                backgroundColor: Color(0XFF56C596),
-                child: Icon(
-                  Icons.person,
-                  color: Color(0XFF205072),
-                  size: 50.0,
+      child: GestureDetector(
+        onLongPress: () {
+          showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.sp),
+            ),
+            backgroundColor: const Color(0XFF205072),
+            context: context,
+            builder: (context) {
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.sp),
+                          child: const Icon(
+                            Icons.person,
+                            color: Colors.white,
+                            size: 50.0,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            Text(
+                              "ID: ${employee.id}",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.sp,
+                              ),
+                            ),
+                            Text(
+                              "Name: ${employee.name}",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.sp,
+                              ),
+                            ),
+                            Text(
+                              "Salary: ${employee.salary}",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.sp,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20.sp),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              employeeFirestoreHelper.deleteEmployee(
+                                  id: employee.id);
+                              Navigator.pop(context);
+                            },
+                            child: const Icon(Icons.delete),
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all<Color>(Colors.red),
+                              fixedSize: MaterialStateProperty.all<Size>(
+                                Size(150.w, 25.w),
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () => {},
+                            child: const Icon(Icons.edit),
+                            style: ButtonStyle(
+                              fixedSize: MaterialStateProperty.all(
+                                Size(150.w, 25.w),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0XFF56C596),
+            borderRadius: BorderRadius.circular(20.sp),
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10.sp),
+                child: const CircleAvatar(
+                  backgroundColor: Color(0XFF56C596),
+                  child: Icon(
+                    Icons.person,
+                    color: Color(0XFF205072),
+                    size: 50.0,
+                  ),
                 ),
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.baseline,
-              textBaseline: TextBaseline.alphabetic,
-              children: [
-                Text(
-                  "ID: ${employee.id}",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.sp,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text(
+                    "ID: ${employee.id}",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp,
+                    ),
                   ),
-                ),
-                Text(
-                  "Name: ${employee.name}",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.sp,
+                  Text(
+                    "Name: ${employee.name}",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp,
+                    ),
                   ),
-                ),
-                Text(
-                  "Salary: ${employee.salary}",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.sp,
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
